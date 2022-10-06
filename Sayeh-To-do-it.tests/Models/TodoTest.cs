e28830484a173f24768b0efd4d5bc50c63a5a045
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-10-06 13:45:22 +0200

    TodoTest is Green.

--- Sayeh-To-do-it.tests/Models/TodoTest.cs
+++ Sayeh-To-do-it.tests/Models/TodoTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sayeh_To_do_it.Data;
+using Sayeh_To_do_it.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace Sayeh_To_do_it.tests.Models
 {
     public class TodoTest
     {
+        [Fact]
+        public void TodoClassTest()
+        {
+            //Arrange
+            string description1 = "Finish assignment";
+            string description2 = "Go for walk";
+
+            //Act
+            Todo testTodo1 = new Todo(TodoSequencer.NextTodoId(),description1);
+            Todo testTodo2 = new Todo(TodoSequencer.NextTodoId(), description2);
+
+            //Assert
+
+            Assert.Equal(description1, testTodo1.Description);
+            Assert.Equal(description2, testTodo2.Description);
+
+
+        }
     }
 }
